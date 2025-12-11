6ba566f93ba30010197daa8aa2fdb8f1275ee855
Denis Kiryukhin <den.kiryukhin@gmail.com>
2025-12-11 13:48:41 +0100

    score reporting complete

--- game_ros/scripts/result_game.py
+++ game_ros/scripts/result_game.py
@@ -2,6 +2,7 @@
 import rospy
 from std_msgs.msg import Int64
 from game_ros.msg import user_msg
+from game_ros.srv import GetUserScore
 
 class ResultGameNode:
     def __init__(self):
@@ -10,11 +11,14 @@ class ResultGameNode:
         self.user_info = None
         self.score = None
 
+        self.scores = {}
+        
         self.user_sub = rospy.Subscriber('user_information', user_msg,
                                          self.user_callback)
         self.result_sub = rospy.Subscriber('result_information', Int64,
                                            self.result_callback)
 
+        self.userscore = rospy.Service("getuserscore", GetUserScore, self.fetchScore)
         rospy.loginfo("RESULT_GAME: waiting for user info and score...")
 
     def user_callback(self, msg):
@@ -31,6 +35,14 @@ class ResultGameNode:
         if self.user_info is not None and self.score is not None:
             print(f"\nGAME OVER! User '{self.user_info.username}' scored {self.score} points.")
             rospy.loginfo("RESULT_GAME: printed result.")
+            self.scores[str(self.user_info.username)] = int(self.score)
+    
+    def fetchScore(self, req):
+        try:
+            if self.scores[req.username]:
+                return int(self.scores[req.username])
+        except:
+            return 0
 
     def run(self):
         rospy.spin()
